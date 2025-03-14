aa700b5bae10b157194ae23fd48423564db9cff0
tonky <tommaso.ticci@stud.unifi.it>
2025-03-14 16:07:07 +0100

    [V0.2] Added Item
    Signed-off-by: tonky <tommaso.ticci@stud.unifi.it>

---
+++ src/main/java/DomainModel/Item.java
@@ -0,0 +1,37 @@
+package main.java.DomainModel;
+
+public class Item {
+    private int itemId;
+    private String name, description, type;
+    private float price;
+    private int discountPercentage;
+
+    //Default constructor
+    public Item() {};
+
+    //Constructor
+    public Item(int itemId, String name, String description, String type, float price, int discountPercentage) {
+        this.itemId = itemId;
+        this.name = name;
+        this.description = description;
+        this.type = type;
+        this.price = price;
+        this.discountPercentage = discountPercentage;
+    }
+
+    //Getters
+    public int getItemId() { return itemId; }
+    public String getName() { return name; }
+    public String getDescription() { return description; }
+    public String getType() { return type; }
+    public float getPrice() { return price; }
+    public int getDiscountPercentage() { return discountPercentage; }
+
+    //Setters
+    public void setItemId(int itemId) { this.itemId = itemId; }
+    public void setName(String name) { this.name = name; }
+    public void setDescription(String description) { this.description = description; }
+    public void setType(String type) { this.type = type; }
+    public void setPrice(float price) { this.price = price; }
+    public void setDiscountPercentage(int discountPercentage) { this.discountPercentage = discountPercentage; }
+}
